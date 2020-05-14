e9c0ff84002fde0b6b63b4f7590e3a94329da088
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-05-14 19:29:41 +0200

    Renderizado

--- main.py
+++ main.py
@@ -1,6 +1,98 @@
 import pygame, sys
 from pygame.locals import *
 
+class Termometro():
+    def __init__(self):
+        self.costume = pygame.image.load("images/termo1.png")
+
+class NumberInput():
+    __value = 0
+    __strValue = "0"
+    __position = [0, 0]
+    __size = [0,0]
+
+    def __init__(self, value=0):
+        self.__font = pygame.font.SysFont("Arial", 24)
+        
+
+    def render(self):
+        textBlock  = self.__font.render(self.__strValue, True, (74, 74, 74))
+        rect = textBlock.get_rect()
+        rect.left = self.__position[0]
+        rect.top = self.__position[1]
+        rect.size = self.__size
+
+        return (rect, textBlock)
+
+
+
+    def value(self, val=None):
+        if val == None:
+            return self.__value
+        else:
+            val = str(val)
+            try:
+                self.__value = int(val)
+                self.__strValue = val
+            except:
+                pass
+
+    def width(self, val=None):
+        if val == None:
+            return self.__size[0]
+        else:
+            try:
+                self.__size[0] = int(val)
+            except:
+                pass
+
+    def height(self, val=None):
+        if val == None:
+            return self.__size[1]
+        else:
+            try:
+                self.__size[1] = int(val)
+            except:
+                pass
+
+    def size(self, val=None):
+        if val == None:
+            return self.__size
+        else:
+            try:
+                w = int(val[0])
+                h = int(val[1])
+                self.__size = [int(val[0]), int(val[1])]
+            except:
+                pass
+
+    def posX(self, val=None):
+        if val == None:
+            return self.__position[0]
+        else:
+            try:
+                self.__position[0] = int(val)
+            except:
+                pass
+
+    def posY(self, val=None):
+        if val == None:
+            return self.__position[1]
+        else:
+            try:
+                self.__position[1] = int(val)
+            except:
+                pass
+
+    def pos(self, val=None):
+        if val == None:
+            return self.__position
+        else:
+            try:
+                self.__position = [int(val[0]), int(val[1])]
+            except:
+                pass
+            
 class mainApp():
     termometro = None
     entrada = None
@@ -10,6 +102,12 @@ class mainApp():
         self.__screen = pygame.display.set_mode((290, 415))
         pygame.display.set_caption("Termómetro")
         self.__screen.fill((244, 236, 203))
+
+        self.termometro = Termometro()
+        self.entrada = NumberInput()
+        self.entrada.pos((106, 58))
+        self.entrada.size((133, 28))
+
 
     def __on_close(self):
         pygame.quit()
@@ -21,6 +119,10 @@ class mainApp():
                 if event.type == QUIT:
                     self.__on_close()
 
+                
+                self.__screen.blit(self.termometro.costume, (50, 34))
+                text = self.entrada.render()
+                pygame.draw.rect(self.__screen, (255, 255, 255), text[0])
                 pygame.display.flip()
 
 if __name__ == '__main__':
